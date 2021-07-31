ad366b878dd1187bdf89f32a1d4feee96c862387
zhalok <zhalokrahman007@gmail.com>
2021-07-31 11:26:34 +0600

    update

--- node.js
+++ node.js
@@ -1,18 +1,51 @@
-const http = require("http");
+// const http = require("http");
+// const fs = require("fs");
+// const page_data = fs.readFileSync("index.html");
+
+
+// //  const server = http.createServer((req,res)=>{
+// //    if(req.url=="/")
+// //    {
+
+// //      res.write(page_data);
+// //      res.end();
+
+// //    }
+// //    else if(req.url=="/welcome"&&req.method=="POST")
+// //    {
+// //      req.on("data",(chunk)=>{
+// //        console.log(chunk.toString());
+// //      })
+// //      res.write("hello");
+// //      console.log("hello")
+// //      res.end();
+// //    }
+// // });
+
+
+
+
+
+// // server.listen(3000);
+// // console.log("Server is running");
+
+// // const app = {}
+// // app.val =2;
+// // console.log(app);
+
+
+const data = require("./data.json");
+const person = {
+    name: "kaifa tabassum",
+    age: 23,
+
+}
+
+data.push(person);
+console.log(typeof JSON.stringify(data));
+
 const fs = require("fs");
-const page_data = fs.readFileSync("index.html");
-
-
-const server = http.createServer((req,res)=>{
-  if(req.url=="/"){
-      res.write(page_data);
-      res.end();
-  }
-});
+fs.writeFileSync("data.json", JSON.stringify(data));
 
 
 
-
-
-// server.listen(3000);
-console.log("Server is running");
---
+++ script.js
@@ -0,0 +1,3 @@
+const obj ={}
+obj.val=2;
+console.log(obj);
