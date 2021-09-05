0789419863869cdfcbd78916afbdacf6850eefd8
zhalok <zhalokrahman007@gmail.com>
2021-09-05 23:46:35 +0600

    uu

--- helpers/enviroments.js
+++ helpers/enviroments.js
@@ -6,6 +6,10 @@ environments.staging = {
     port: 3000,
     envName: 'staging',
     secretKey: 'hsjdhsdhsjdhjshdjshd',
+    twilio: {
+        sid: "ACd5d59be78c41fc8c56ae86641a979640",
+        token: "9ce5c1744b1835358e929fc446fc476d",
+    }
 };
 
 // production environment
@@ -13,6 +17,10 @@ environments.production = {
     port: 5000,
     envName: 'production',
     secretKey: 'djkdjskdjksdjksjdskjd',
+    twilio: {
+        sid: "ACd5d59be78c41fc8c56ae86641a979640",
+        token: "9ce5c1744b1835358e929fc446fc476d",
+    }
 };
 
 // determine which environment was passed
